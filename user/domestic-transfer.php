9746f32f566ac656e861fde105a3aea569cbe427
xmartina <martinsprince126@gmail.com>
2024-12-11 04:53:41 +0100

    d

--- user/domestic-transfer.php
+++ user/domestic-transfer.php
@@ -145,7 +145,7 @@ require_once("./userPinfunction.php");
 
                                         <div class="row">
                                             <div class="col-md-12 text-center">
-                                                <button class="btn btn-primary mb-2 mr-2" name="domestic-pin"><svg
+                                                <button class="btn btn-primary mb-2 mr-2" name="domestic-transfer"><svg
                                                         xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                                         viewBox="0 0 24 24" fill="none" stroke="currentColor"
                                                         stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
