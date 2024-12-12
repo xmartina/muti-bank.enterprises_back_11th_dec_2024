63a9054d0ac91100f45d73b11405c9b5cb8bc012
xmartina <martinsprince126@gmail.com>
2024-12-12 13:11:33 +0100

    d

--- user/pin.php
+++ user/pin.php
@@ -60,7 +60,7 @@ include("./userPinfunction.php");
                                 </div>
                                 <div class="col-md-6 ">
                                     <div class="form-group ">
-                                        <button class="btn btn-primary col-12" type="submit" >Submit</button>
+                                        <button class="btn btn-primary col-12" name="submit-pin" type="submit" >Submit</button>
                                     </div>
                                 </div>
                             </div>
