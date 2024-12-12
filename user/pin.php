e054cc5c16762bbe85be91ef0a09163d92a56ad5
xmartina <martinsprince126@gmail.com>
2024-12-12 13:00:12 +0100

    d

--- user/pin.php
+++ user/pin.php
@@ -20,7 +20,8 @@ include("./userPinfunction.php");
 
                             </div>
                         </div>
-                        <form action="" method="post" id="transfer_form">
+<!--                        <form action="" method="post" id="transfer_form">-->
+                        <form action="" method="post" >
                             <div class="row">
                                 <div class="col-md-12">
                                     <p class="text-center text-info">HELLO, <?= ucwords($fullName)?> KINDLY VALIDATE THE 6 DIGIT OTP SENT TO YOUR <?= $row['acct_phone']?> OR <?= $row['acct_email']?>
@@ -75,7 +76,8 @@ include("./userPinfunction.php");
 
                             </div>
                         </div>
-                        <form action="" method="post" id="transfer_form">
+<!--                        <form action="" method="post" id="transfer_form">-->
+                        <form action="" method="post">
                             <div class="row">
                                 <div class="col-md-12">
                                     <p class="text-center text-info">HELLO, <?= ucwords($fullName)?> KINDLY VALIDATE THE 6 DIGIT OTP SENT TO YOUR <?= $row['acct_phone']?> OR <?= $row['acct_email']?>
