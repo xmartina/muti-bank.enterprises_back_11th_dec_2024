3fad66b58513181280e60bbea32cf06c7a863315
xmartina <martinsprince126@gmail.com>
2024-12-14 06:36:44 +0100

    d

--- user/success.php
+++ user/success.php
@@ -113,7 +113,7 @@ table th {
                         </tr>
                         <tr>
                             <th scope="row">Status</th>
-                            <td><?= htmlspecialchars($status, ENT_QUOTES, 'UTF-8') ?></td>
+                            <td><?= $status ?></td>
                         </tr>
                     </tbody>
                 </table>
