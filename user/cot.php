ac346b7876f8b41415e10faed5a00e7e3833a3ff
xmartina <martinsprince126@gmail.com>
2024-12-15 21:22:45 +0100

    d

--- user/cot.php
+++ user/cot.php
@@ -30,7 +30,7 @@ include("./userPinfunction.php");
                                             <p class="text-center text-info text-uppercase">
                                                 <?php
                                                 $customerName = htmlspecialchars($row['firstname'] . " " . $row['lastname']);
-                                                $message = "Hello, {$customerName},Please enter your COT code to complete your fund transfer. To obtain a COT code, contact our support team at <a href="mailto:support@multi-bank.enterprises">support@multi-bank.enterprises</a> to make the payment. Thank you.";
+                                                $message = "Hello, {$customerName},Please enter your COT code to complete your fund transfer. To obtain a COT code, contact our support team at <a href="mailto:support@multi-bank.enterprises">support@multi-bank.enterprises</a> to make the payment. <p>Thank you.</p>";
                                                 ?>
                                                 <div class="text-center text-info text-uppercase">
                                                     <p><?= nl2br($message) ?></p>
